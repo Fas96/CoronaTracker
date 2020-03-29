b1a6112419d3b12570f46dc0ee2e652fdcbe8b58
muratcanbur <muratcanbur@gmail.com>
2020-03-29 15:30:24 +0300

    add application screenshots

--- app/src/main/java/co/icanteach/app/coronatracker/presentation/news/NewsFragment.kt
+++ app/src/main/java/co/icanteach/app/coronatracker/presentation/news/NewsFragment.kt
@@ -3,14 +3,12 @@ package co.icanteach.app.coronatracker.presentation.news
 import android.content.Context
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.browser.customtabs.CustomTabsIntent
 import androidx.core.content.ContextCompat
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import co.icanteach.app.coronatracker.CoronaTrackerApplication
 import co.icanteach.app.coronatracker.R
@@ -22,7 +20,7 @@ import javax.inject.Inject
 
 class NewsFragment : Fragment() {
 
-    lateinit var newsComponent: NewsComponent
+    private lateinit var newsComponent: NewsComponent
 
     private val viewModel: NewsViewModel by lazy(LazyThreadSafetyMode.NONE) {
         ViewModelProvider(this, viewModelFactory).get(NewsViewModel::class.java)
